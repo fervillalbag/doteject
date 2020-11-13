6737238f306e93ebe686147efc5e11a928187997
fervillalbag <21fervillalbag@gmail.com>
2020-11-13 14:59:15 -0300

    Finishing the novedades section

--- js/app.js
+++ js/app.js
@@ -5,3 +5,28 @@ const btnBars = document.querySelector('#btn-bars');
 btnBars.addEventListener('click', () => {
    navigationMenu.classList.toggle('active');
 })
+
+/* Popup Novedades */
+
+const novedadesBtnPost = document.querySelectorAll('.novedades__link');
+
+const novedadesBtnClose = document.querySelector('#novedadesBtnClose');
+const novedadesPopup = document.querySelector('#novedadesPopup');
+const novedadesOverlay = document.querySelector('#novedadesOverlay');
+
+novedadesBtnPost.forEach(btnPost => {
+   btnPost.addEventListener('click', () => {
+      novedadesOverlay.classList.add('active');
+      novedadesPopup.classList.add('active');
+   });
+});
+
+novedadesBtnClose.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+});
+
+novedadesOverlay.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+})
