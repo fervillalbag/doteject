fa3dfb1f2bdfb516db9f4f45845af7afdfc39ee9
fervillalbag <21fervillalbag@gmail.com>
2020-11-16 09:52:18 -0300

    Finishing postularte button

--- js/app.js
+++ js/app.js
@@ -22,3 +22,28 @@ menuOverlay.addEventListener('click', (e) => {
    dropdownBody1.classList.remove('active');
    menuOverlay.classList.remove('active');
 });
+
+/* Popup Novedades */
+
+const novedadesBtnPost = document.querySelectorAll('.novedades__link');
+
+const novedadesBtnClose = document.querySelector('#novedadesBtnClose');
+const novedadesPopup = document.querySelector('#novedadesPopup');
+const novedadesOverlay = document.querySelector('#novedadesOverlay');
+
+novedadesBtnPost.forEach(btnPost => {
+   btnPost.addEventListener('click', () => {
+      novedadesOverlay.classList.add('active');
+      novedadesPopup.classList.add('active');
+   });
+});
+
+novedadesBtnClose.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+});
+
+novedadesOverlay.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+});
