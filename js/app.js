a37ef0e5c7ef0f5ebff5cfb499917d98b9d5f07d
fervillalbag <21fervillalbag@gmail.com>
2020-11-16 09:07:14 -0300

    Finishing the dropdown and links

--- js/app.js
+++ js/app.js
@@ -6,27 +6,19 @@ btnBars.addEventListener('click', () => {
    navigationMenu.classList.toggle('active');
 })
 
-/* Popup Novedades */
+/* Dropdown */
 
-const novedadesBtnPost = document.querySelectorAll('.novedades__link');
+const dropdown1 = document.querySelector('#dropdown1');
+const dropdownBody1 = document.querySelector('#dropdownBody1');
+const menuOverlay = document.querySelector('#menuOverlay');
 
-const novedadesBtnClose = document.querySelector('#novedadesBtnClose');
-const novedadesPopup = document.querySelector('#novedadesPopup');
-const novedadesOverlay = document.querySelector('#novedadesOverlay');
-
-novedadesBtnPost.forEach(btnPost => {
-   btnPost.addEventListener('click', () => {
-      novedadesOverlay.classList.add('active');
-      novedadesPopup.classList.add('active');
-   });
+dropdown1.addEventListener('click', () => {
+   dropdownBody1.classList.add('active');
+   menuOverlay.classList.add('active');
 });
 
-novedadesBtnClose.addEventListener('click', () => {
-   novedadesPopup.classList.remove('active');
-   novedadesOverlay.classList.remove('active');
-});
-
-novedadesOverlay.addEventListener('click', () => {
-   novedadesPopup.classList.remove('active');
-   novedadesOverlay.classList.remove('active');
+menuOverlay.addEventListener('click', (e) => {
+   e.preventDefault();
+   dropdownBody1.classList.remove('active');
+   menuOverlay.classList.remove('active');
 });
---
+++ js/popup.js
@@ -0,0 +1,24 @@
+/* Popup Novedades */
+
+const novedadesBtnPost = document.querySelectorAll('.novedades__link');
+
+const novedadesBtnClose = document.querySelector('#novedadesBtnClose');
+const novedadesPopup = document.querySelector('#novedadesPopup');
+const novedadesOverlay = document.querySelector('#novedadesOverlay');
+
+novedadesBtnPost.forEach(btnPost => {
+   btnPost.addEventListener('click', () => {
+      novedadesOverlay.classList.add('active');
+      novedadesPopup.classList.add('active');
+   });
+});
+
+novedadesBtnClose.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+});
+
+novedadesOverlay.addEventListener('click', () => {
+   novedadesPopup.classList.remove('active');
+   novedadesOverlay.classList.remove('active');
+});
